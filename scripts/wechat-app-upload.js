7586f8859b25ed7eca2366c74c4c95613deee0c1
DESKTOP-7PDFHTN\mytac <my_tac@126.com>
2019-07-02 17:39:52 +0800

    :boom: 小程序命令行上传代码

--- scripts/wechat-app-upload.js
+++ scripts/wechat-app-upload.js
@@ -4,12 +4,20 @@ const path = require('path')
 const fs = require('fs')
 const readline = require("readline")
 const cp = require("child_process")
+const ChatBot = require('dingtalk-robot-sender');
 
 const CONFIG = {
     CLI_PATH: 'C:/微信web开发者工具/cli.bat',
     PROJECT_PATH: '/workspace/GpWxapp/dist',
     OUTPUT_PATH: '/workspace/GpWxapp/info.json'
 }
+
+const robot = new ChatBot({
+    webhook: 'xxxxx'
+});
+
+
+
 
 // open IDE
 function init() {
@@ -35,11 +43,11 @@ function input() {
             output: process.stdout
         })
 
-        rl.question('请输入版本号和备注，以空格分隔： \n', answer => {
+        rl.question('请输入备注和版本号，以空格分隔（版本号可不填默认为git tag latest）： \n', answer => {
             rl.close();
             if (answer.includes(' ')) {
-                const [version, comment] = answer.split(' ')
-                if ((!version || !comment || !version.length || !comment.length)) {
+                const [comment, version] = answer.split(' ')
+                if ((!comment || !comment.length)) {
                     console.log('输入不合法，请重新输入')
                     resolve(input())
                 } else {
@@ -47,7 +55,8 @@ function input() {
                     resolve({ version, comment })
                 }
             } else {
-                reject('请重新输入')
+                const comment = answer
+                resolve({ comment })
             }
         })
     })
@@ -55,24 +64,100 @@ function input() {
 
 // upload your code.
 function upload(version, comment) {
+    console.log('version', version, 'comment', comment)
     return new Promise((resolve, reject) => {
         const cmd = `cli -u ${version}@${CONFIG.PROJECT_PATH} --upload-desc ${comment}`
         cp.execFile(CONFIG.CLI_PATH, cmd.split(' '), (error, stdout, stderr) => {
             if (error) {
                 console.log('Failed to start IDE and open the project.')
                 reject(error);
+            } else {
+                console.log(stdout);
+                console.log('上传完成')
+                resolve(`驿站团小程序上传完成:${comment} - （版本号：${version}）`);
             }
-            console.log(stdout);
-            resolve();
         })
     })
 }
 
+// 提交git信息并加标签
+async function gitCommit(version, commit, isUpdateTag = true) {
+    cp.execSync('git add .')
+    return new Promise((resolve, reject) => {
+        cp.exec(`git tag`, (err, stdout, stderr) => {
+            if (err) {
+                reject(err)
+            } else {
+                const tags = stdout.split('\n')
+                resolve(tags[tags.length - 2]);
+            }
+        })
+    }).then((tag) => {
+        if (tag) {
+            const oldTag = tag
+            const [a, b, c] = oldTag.split('.')
+            const newTag = isUpdateTag ? (version && version.length ? version : [a, b, Number(c) + 1 || 0].join('.')) : oldTag;
+            console.log('您的版本号为：', newTag, '您的提交信息为：', commit, '旧版本号为：', oldTag)
+            return new Promise((resolve, reject) => {
+                cp.exec(`git commit -m ${commit}`, (err, stdout, stderr) => {
+                    if (err) {
+                        console.log(err)
+                        reject(err)
+                    } else {
+                        if (isUpdateTag) {
+                            cp.execSync(`git tag ${newTag}`)
+                        }
+                        resolve(newTag)
+                    }
+                })
+            })
+
+        }
+    })
+        .catch((err) => {
+            console.log('gitcommit 错误')
+            throw err
+        })
+
+
+}
+
+// 通知钉钉群
+async function notify(msg, noAt = false) {
+    let textContent = {
+        "msgtype": "text",
+        "text": {
+            "content": msg
+        },
+        "at": {
+            "atMobiles": [
+                "xxxxxxxxxx",
+                "xxxxxxxxxx"
+            ],
+            "isAtAll": false
+        }
+    }
+
+    if (noAt) {
+        delete textContent.at
+    }
+
+    await robot.send(textContent)
+
+}
+
+
 async function main() {
     try {
+        const params = process.argv.slice(2)
+        const isUpdateTag = !params.includes('-notag')
+        const noAt = !params.includes('-noAt')
         await init();
         const { version, comment } = await input();
-        await upload(version, comment)
+        const newTag = await gitCommit(version, comment, isUpdateTag)
+        const msg = await upload(newTag, comment)
+        await notify(msg, noAt)
+        console.log('推送完毕!')
     } catch (e) {
         console.log('e', e)
     }
